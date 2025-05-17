908103201cf8f5cc311f315dbe1f87ac1276b998
AlexWang1017 <alexwang496@gmail.com>
2025-05-18 01:15:31 +0800

    upload file

---
+++ C++/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x ;
+    cout << "Enter an integer:"; cin >> x;
+    cout << x << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ C++/varType.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int age = 20;
+    float height = 182.1;
+    char grade = 'A';
+    bool passed = true;
+    string name = "Alex";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "height: " << height << endl;
+    cout << "grade: " << grade << endl;
+    cout << "passed: " << passed << endl;
+
+
+
+   
+    return 0;
+}
