d0086dc2eefb480bfb1c0a927bc13a13abfe4503
AlexWang1017 <alexwang496@gmail.com>
2025-05-18 02:31:24 +0800

    upload file

--- C++/randNum.cpp
+++ C++/randNum.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 
 int main(){
+    srand(time(0));
     int ans = rand() % 100 + 1;
     int num = 0;
     
-    while(num != num){
+    while(num != ans){
         cin >> num;
         if (num > ans){
             cout << "It's larger than the answer" << endl;
         }
-        if (num < ans){
+        else if (num < ans){
             cout << "It's smaller than the answer" << endl;
         }
-    }   
+    }  
+    cout << "You got the right answer" << endl;
     system("pause");
     return 0;
 }
